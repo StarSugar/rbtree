16556cb763e7804382b2fe8a70e79f06ded13f56
StarSugar <92320877+StarSugar@users.noreply.github.com>
2023-01-20 17:13:15 +0800

    Update rbtree_types.h

--- rbtree_types.h
+++ rbtree_types.h
@@ -1,4 +1,6 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+// SPDX-License-Identifier: GPL-2.0-or-later
+// This file is copied and edited from linux kernel.
+
 #ifndef _LINUX_RBTREE_TYPES_H
 #define _LINUX_RBTREE_TYPES_H
 
